38515c57834436265d64c6978a4cd2b4b9fcf7f0
ulvieyvazov <eyvazovulvi010101@gmail.com>
2023-05-15 08:08:33 +0400

    add

--- src/components/Countries/countries.jsx
+++ src/components/Countries/countries.jsx
@@ -5,38 +5,44 @@ import { useNavigate } from "react-router-dom";
 
 const Countries = () => {
   const navigate = useNavigate();
+  const [value, setValue] = useState('');
   const [data, setData] = useState([]);
-  const [value, setValue] = useState("")
 
-  
   // const onCheck = (e) => {
   //   setValue(e.target.value)
   //   console.log(value);
   // }
 
-  
-  
-  
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const res = await axios.get(`https://restcountries.com/v3.1/all`);
-  //     setData(res.data);
-  //     setValue( data.filter(item =>item.name.common.toLowerCase().includes(value.toLowerCase())))
-  //     setValue(value);
-  //   };
-  //   getData();
-  // }, [value]);
+  const getData = async () => {
+    const res = await axios.get(`https://restcountries.com/v3.1/all`);
+    // setData(res.data);
+    setData(
+      res.data.filter((item) =>
+        item.name.common.toLowerCase().includes(value.toLowerCase())
+      )
+    );
+    // setValue(value);
+  };
 
+  useEffect(() => {
+    getData();
+  }, [value]);
 
   return (
     <>
-      <input type="text" onChange={(e=>setValue(e.target.value))} />
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
       <div className="container">
         {data
           // .filter(f => (f.population > 10000000))
           .map((d) => (
             <div className="cart">
-              <img key={d.name.common} onClick={() => navigate(`name/${d.name.common}`)}
+              <img
+                key={d.name.common}
+                onClick={() => navigate(`name/${d.name.common}`)}
                 src={d.flags.png}
                 alt=""
               />
